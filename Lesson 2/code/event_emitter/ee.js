c6760a7910c3e6df0ce8487489acd00e6e07d144
Malina <nm76006@st.amu.edu.pl>
2014-10-18 17:13:05 +0200

    homework - event emitter

--- Lesson 2/code/event_emitter/ee.js
+++ Lesson 2/code/event_emitter/ee.js
@@ -11,25 +11,47 @@
 	};
 
 	EE.prototype.on = function (eventName, listener, context) {
-
+	        if(this.listeners[eventName]){
+	                this.listeners[eventName].push(listener.bind(context));
+	        }
+                else{
+                        this.listeners[eventName] = [listener.bind(context)];
+                }
+		//console.log("on "+ eventName );
+		
 	};
 
 	EE.prototype.emit = function (eventName /*, other args...*/) {
+		var listenersTab = this.listeners[eventName];
+		var args = Array.slice(arguments,1);
+	        
+		for(i in listenersTab){
+		        listenersTab[i].apply(this, args);
+		}
 
+		
 	};
 
-//	var ee = new EE();
-//
-//	var removeListener = ee.on('test', function (arg1, arg2) {
-//		console.log(arg1, arg2, this.key);
-//	}, { key: 'value' });
-//
-//	ee.emit('test', 1, 2); // 1, 2 value
+	var ee = new EE();
+
+        //ee.emit('ev', "argument1", "argument2");
+        
+	var removeListener = ee.on('test', function (arg1, arg2) {
+		console.log(arg1, arg2, this.key);
+	}, { key: 'value' });
+
+	
+	ee.on('test', function () {
+		console.log("drugi listener");
+	});
+	
+	ee.emit('test', 1, 2); // 1, 2 value
 //
 //	removeListener(); //removes my listener from the event emitter;
 //
 //	ee.emit('test'); //nothing will execute
-
+        
+        global.UAM.eee = ee;
 	global.UAM.EventEmitter = EE;
 
 }(window));
