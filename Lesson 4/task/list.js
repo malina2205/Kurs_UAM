39f4078db619d7aa7b7cb9a316769ac44648deb4
Malina <nm76006@st.amu.edu.pl>
2014-11-04 16:51:48 +0100

    ugly list

---
+++ Lesson 4/task/list.js
@@ -0,0 +1,28 @@
+window.onload = function() {
+        var list = document.getElementById('list');
+        var input = document.getElementById('input_text');
+        var tab = [];
+        document.getElementById('add_button').addEventListener('click', function () {
+                var text = input.value;
+                if(text !=""){
+                        input.style.borderColor = "black";
+                        var listItem = document.createElement('li');
+                        listItem.innerHTML = text  + ' <span class="delete">Delete</span>';
+                        list.appendChild(listItem);
+                        tab.push(listItem);
+                        input.value = ""
+                }
+                else{
+                        input.style.borderColor = "red";
+                }
+        });
+
+        list.addEventListener('click', function (elem) {
+                 var target = elem.target;
+                 if (target.className == "delete"){
+                        target.parentElement.remove();
+                        tab.splice(tab.indexOf(target.parentElement),1);
+                 }
+                        
+        });
+}
